4e71396ea1d27af8f791865223a585afa1aef596
Yurii Kovalchuk <kovalchuk.yurii@hneu.net>
2026-01-05 15:50:05 +0000

    Add hw_3.2

---
+++ hw_3.2.py
@@ -0,0 +1,20 @@
+ex1 = [12, 3, 4, 10]
+ex2 = [1]
+ex3 = []
+ex4 = [12, 3, 4, 10, 8]
+
+if ex1:
+    ex1.insert(0, ex1.pop())
+print(ex1)
+
+if ex2:
+    ex2.insert(0, ex2.pop())
+print(ex2)
+
+if ex3:
+    ex3.insert(0, ex3.pop())
+print(ex3)
+
+if ex4:
+    ex4.insert(0, ex4.pop())
+print(ex4)
